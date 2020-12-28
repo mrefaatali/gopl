32ccdbf9092982c2dca647d2421febb5cc3cf042
Mohamed Refaat Ali <engmrefaatali@gmail.com>
2020-12-28 07:45:45 +0000

    Initial commit

---
+++ ch5/outline/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "outline: %v\n", err)
+		os.Exit(1)
+	}
+	forEachNode(doc, startElement, endElement)
+}
---
+++ ch5/outline/outline.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/net/html"
+)
+
+func outline(stack []string, n *html.Node) {
+	if n.Type == html.ElementNode {
+		stack = append(stack, n.Data)
+		fmt.Println(stack)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		outline(stack, c)
+	}
+}

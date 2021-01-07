16307ee7da8233213882b8a17096131541ef9ae8
Mohamed Refaat Ali <engmrefaatali@gmail.com>
2021-01-07 20:56:13 +0000

    clock2 ch8 and netcat1

---
+++ ch8/clock2/clock2.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	listner, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listner.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn) //handles connections concurrently
+	}
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().Format("Mon 15:04:05.000\n"))
+		if err != nil {
+			return
+		}
+		time.Sleep(500 * time.Millisecond)
+	}
+}
